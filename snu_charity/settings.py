d981565d185664a656e9e86343ecbdb68cb4e96b
Qria <qria@qria.net>
2020-05-17 20:19:23 +0900

    Set up simple template

--- snu_charity/settings.py
+++ snu_charity/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'snu-charity-auction.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'auction.apps.AuctionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
