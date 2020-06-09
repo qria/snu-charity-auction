7575cb91322e5413cc6a5e39cfb28da10c6c6bf8
Qria <qria@qria.net>
2020-06-09 19:40:04 +0900

    Implement login

--- auction/urls.py
+++ auction/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- snu_charity/settings.py
+++ snu_charity/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 # setup static file serving for heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL = 'index'
